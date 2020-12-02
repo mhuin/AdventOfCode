f51c04a27e50f09ea96b6aec1276aed008eeeecd
Matthieu Huin <mhuin@redhat.com>
2020-12-02 10:19:58 +0100

    day 1-2: Time results

--- 2020/day01/day1-2.py
+++ 2020/day01/day1-2.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 with open('input') as rawinput:
     _values = [int(x) for x in rawinput.readlines()]
@@ -8,12 +9,13 @@ values = sorted(_values)
 magicsum = 2021
 d = len(values)
 
-print("%s values to process" % d)
+#print("%s values to process" % d)
 
 i, j, k = 0, 1, d
 
 iterations = 0
 
+start = time.time()
 while i < j < len(values):
     if magicsum == 2020:
         break
@@ -23,7 +25,7 @@ while i < j < len(values):
             iterations += 1
             break
         elif magicsum > 2020:
-            print("we're too high, decrease k")
+#            print("we're too high, decrease k")
             k -= 1
             iterations += 1
             break
@@ -34,18 +36,19 @@ while i < j < len(values):
                 if magicsum == 2020:
                     break
                 elif magicsum < 2020:
-                    print("too low, increase i")
+#                    print("too low, increase i")
                     i += 1
                 else:
-                    print("too high, increase j and reset i")
+#                    print("too high, increase j and reset i")
                     j += 1
                     i = 0
                     break
             if i == j:
-                print("completed inner loop, increase j and reset i")
+#                print("completed inner loop, increase j and reset i")
                 j += 1
                 i = 0
+stop = time.time()
 
-print("Result found in %s iterations" % iterations)
+print("Result found in %s iterations, %.3f ms" % (iterations, (stop-start)*1000.))
 print('%s + %s + %s = %s' % (values[i], values[j], values[k-1], values[i] + values[j] + values[k-1]))
 print('%s x %s x %s = %s' % (values[i], values[j], values[k-1], values[i]*values[j]*values[k-1]))
